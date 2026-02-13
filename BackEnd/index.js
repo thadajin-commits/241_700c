622ae912c61112c8a7582b2d1f3f9abcf02472e6
thadajin-commits <thada.jin@ku.th>
2026-02-13 12:18:45 +0700

    add api get post put delete

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,4 +1,5 @@
 //ทำการ import โมดูล Http
+/*
 const http = require('http');
 const host = 'localhost';
 const port = 8000;
@@ -16,3 +17,77 @@ const server = http.createServer(requirelistener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 }); 
+*/
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 8000; 
+
+let users = [];
+let counter = 1;
+
+
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1;
+
+    users.push(user);
+    res.json({
+    message: 'User added successfully',
+    user: user
+    });
+});
+
+app.patch('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updateUser = req.body;
+
+    let selectedIndex = users.findIndex(user => user.id == id );
+
+    users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname;
+    users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname;
+
+    if (updateUser.firstname) {
+        users[selectedIndex].firstname = updateUser.firstname;
+    }
+    if (updateUser.lastname) {
+        users[selectedIndex].lastname = updateUser.lastname;
+    }
+
+    res.json({
+        message: 'User update successfully',
+        data: {
+            user: updateUser,
+            indexUpdate: selectedIndex
+        }
+    });
+})
+
+app.delete('/users/:id', (req, res) => {
+    let id = req.params.id;
+    let selectedIndex = users.findIndex(user => user.id == id );
+
+    users.splice(selectedIndex, 1);
+
+    res.json({
+        message: 'User deleted successfully',
+        indexDelete: selectedIndex
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+
+});
+
