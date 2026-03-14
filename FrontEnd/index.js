61e0b9133bf27150d73ee63035e3a2ca20862367
thadajin-commits <thada.jin@ku.th>
2026-03-14 13:04:47 +0700

    css add html

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,3 +1,59 @@
+const BASE_URL = 'http://localhost:8000';
+let mode = 'CREATE'; //เพิ่มข้อมูล
+let selectedId = '';
+
+
+window.onload = async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    console.log('id:', id);
+    if (id){
+    
+        mode = 'EDIT';
+        selectedId = id;
+        
+        //1.ดึงข้อมูล user เก่ามาเเสดง
+    try { 
+        const response = await axios.get(`${BASE_URL}/users/${id}`);
+        console.log('response:', response.data);
+        const user = response.data;
+        //2.จะนำข้อมูล user ที่ได้มาแสดงในฟอร์ม เพื่อให้เเก้ไขข้อมูลได้
+         let firstnameDOM = document.querySelector('input[name="firstname"]');
+         let lastnameDOM = document.querySelector('input[name="lastname"]');
+         let ageDOM = document.querySelector('input[name="age"]');
+         let descriptionDOM = document.querySelector('textarea[name="description"]');
+           
+         firstnameDOM.value = user.firstname;
+         lastnameDOM.value = user.lastname;
+         ageDOM.value = user.age;
+         descriptionDOM.value = user.description;
+      
+
+         let genderDOMs = document.querySelectorAll('input[name="gender"]');
+         let interestDOMs = document.querySelectorAll('input[name="interests"]');
+        
+          for (let i = 0; i < genderDOMs.length; i++) {
+            if (genderDOMs[i].value === user.gender) {
+                genderDOMs[i].checked = true;
+            }
+         }
+         for (let i = 0; i < interestDOMs.length; i++) {
+            if (user.interests.includes(interestDOMs[i].value)) {
+                interestDOMs[i].checked = true;
+            }
+        }
+
+
+    }catch(error){
+        console.error('Error',error);
+         
+    }
+        
+    
+    
+}
+}
+
 
 const validateData = (userData) => {
     let errors = [];
@@ -55,10 +111,22 @@ if (errors.length > 0) {
         errors: errors
     }
 }
+let message = 'บันทึกข้อมูลสำเร็จ'
 
-const response = await axios.post('http://localhost:8000/users', userData);
-console.log('response:', response);
-messageDOM.innerText = 'บันทึกข้อมูลสำเร็จ';
+
+if (mode == 'EDIT') {
+    const response = await axios.put(`${BASE_URL}/users`,userData);
+    console.log('response:', response);
+
+} else {
+    const response = await axios.post(`${BASE_URL}/users/${selectedId}`,userData);
+    message = 'แก้ไขข้อมูลสำเร็จ';
+    console.log('response:', response.data);
+
+}
+
+
+messageDOM.innerText = message;
 messageDOM.className = 'message success';
 } catch (error) {
     console.log('Error message', error.message);
@@ -70,8 +138,6 @@ messageDOM.className = 'message success';
         error.errors = error.response.data.errors;
     
    }    
-
-   
 
 let htmlData = '<div>';
 htmlData += `<div>${error.message}</div>`;
